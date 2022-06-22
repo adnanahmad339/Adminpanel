b346e058befada27671aee723da894c5ab9b41a7
Adnan Ahmad <adnanahmadtsil@gmail.com>
2022-06-22 06:54:53 +0500

    node version

--- src/Components/Schemas/LoginSchema.js
+++ src/Components/Schemas/LoginSchema.js
@@ -1,3 +1,4 @@
+
 import * as yup from "yup";
 
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
---
+++ src/config.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const axiosInstance= create({
+
+    baseURL:"https://fa18-bce-030-admin.herokuapp.com/api/"
+
+
+
+
+
+
+})
